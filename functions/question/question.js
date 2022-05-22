aea4d2e56a3374feff30c52ce32731293a9575a3
nice2h <me@nice2have.jp>
2022-05-22 16:49:23 +0900

    Hashid on function

--- functions/question/question.js
+++ functions/question/question.js
@@ -1,10 +1,12 @@
 const axios = require("axios");
+const Hashids = require("hashids/cjs");
 class Question {
     constructor() {
         this.admin = require("firebase-admin");
         if (this.admin.apps.length === 0) {
             this.admin.initializeApp();
         }
+        this.database = this.admin.database();
         this.firestore = this.admin.firestore();
         this.firestoreTimestamp = this.admin.firestore.Timestamp;
     }
@@ -13,6 +15,10 @@ class Question {
         try {
             console.log("create");
             console.log(data);
+            const hashids = new Hashids("ankeyto", 8);
+            const id = hashids.encode(100);
+            console.log(id);
+            await this.database.ref("quesitons/" + id).set(data);
             //データベースに登録
             //URL生成
             //結果を返却
